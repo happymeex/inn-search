847fbd92f25e73a8d074a9c4271e1207791ea582
happymeex <32947316+happymeex@users.noreply.github.com>
2023-05-11 10:32:56 -0400

    server web scraping

---
+++ server/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import { allText } from "./utils";
+import fs from "fs";
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, "..", "index.html"));
+});
+
+const staticPath = path.resolve(__dirname, "dist");
+app.use(express.static(staticPath));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+void allText(10, 60 * 10);
---
+++ server/utils.ts
@@ -0,0 +1,124 @@
+import assert from "assert";
+import fs from "fs";
+
+const TABLE_OF_CONTENTS = "https://wanderinginn.com/table-of-contents/";
+const URL_BASE = "https://wanderinginn.com";
+const ignore = ["/vol-1-archive", "/contacts/"];
+
+/**
+ * Writes each chapter to a file data/i.txt
+ *
+ * @param rateLimit maximum number of chapters to handle at a time
+ * @param pauseTime seconds to pause between batches of requests
+ */
+export async function allText(rateLimit = 10000, pauseTime = 0): Promise<void> {
+    const map = await chapterToURL();
+    const chapters: string[] = [];
+    const promises: Promise<string>[] = [];
+
+    /** can't do this all concurrently; run into 429 too many requests */
+    const entries = [...map.entries()];
+    for (const [i, [chapter, url]] of entries.entries()) {
+        console.log("starting chapter", i);
+        chapters.push(chapter);
+        promises.push(chapterText(url));
+        if ((i + 1) % rateLimit === 0 || i + 1 === entries.length) {
+            await writeToFile(rateLimit * Math.floor(i / rateLimit));
+            console.log("finished batch, waiting now...");
+            await wait(pauseTime * 1000);
+            chapters.length = 0;
+            promises.length = 0;
+        }
+    }
+    async function writeToFile(alreadyWritten: number) {
+        console.log("writing batch to file, already written", alreadyWritten);
+        return (
+            Promise.all(promises)
+                // format chapter text by appending chapter name to the top
+                .then((res) => {
+                    return res.map((text, i) => {
+                        const chapter = chapters[i];
+                        assert(chapter);
+                        return chapter + "\n" + text;
+                    });
+                })
+                .then((res) =>
+                    Promise.all(
+                        res.map((chapter, i) =>
+                            fs.writeFile(
+                                `data/${alreadyWritten + i}.txt`,
+                                chapter,
+                                (err) => {
+                                    if (err !== null) console.log(err);
+                                }
+                            )
+                        )
+                    )
+                )
+        );
+    }
+}
+
+/**
+ * @returns a promise to a map from chapter names to relative URLs
+ */
+async function chapterToURL(): Promise<Map<string, string>> {
+    const regex = /<a[\s]+href="(\/[-\w\/]+)">([\w\-\u2013\(\). ]+)<\/a>/g;
+    const res = await fetch(TABLE_OF_CONTENTS);
+    const rawHTML = await res.text();
+    const matches = [...rawHTML.matchAll(regex)];
+    const ret = new Map<string, string>();
+    matches.forEach((match) => {
+        assert(match[1] && match[2]);
+        if (ignore.includes(match[1])) return;
+        ret.set(match[2], match[1]);
+    });
+    return ret;
+}
+
+/**
+ *
+ * @param url a relative url to a chapter
+ * @returns a promise for the text of the corresponding chapter as a single string,
+ *      paragraphs separated by double newlines
+ */
+async function chapterText(url: string): Promise<string> {
+    console.log("looking at url:", url);
+    const regex = new RegExp(
+        /<div[\s]+class[\s]*=[\s]*"entry-content">(.*?)<\/div>/,
+        "s"
+    );
+    try {
+        const res = await fetch(URL_BASE + url);
+        const rawHTML = await res.text();
+        const match = rawHTML.match(regex);
+        if (!match || !match[1]) {
+            console.log(rawHTML);
+            console.log("error getting content div for chapter", url);
+            throw new Error();
+        }
+        return extractText(match[1]);
+    } catch (err) {
+        console.log("errored on:", url, "error was:", err);
+        return "";
+    }
+}
+
+/**
+ * @param rawHTML raw HTML between the <div class="entry-content"> of a chapter's source HTML
+ * @returns the text of the chapter as a single string with paragraps separated by double newlines
+ */
+function extractText(rawHTML: string): string {
+    const regex = new RegExp(/<p>(.*?)<\/p>/, "sg");
+    const m = [...rawHTML.matchAll(regex)];
+    return m
+        .map((match) => {
+            assert(match[1]);
+            return match[1];
+        })
+        .join("\n\n");
+}
+
+function wait(msec: number): Promise<void> {
+    return new Promise((resolve, _) => setTimeout(resolve, msec));
+}
